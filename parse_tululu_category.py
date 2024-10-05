bff97465a60dc090a63178964c89acbfefffc354
Stepan Makarov <st.mkv03@gmail.com>
2024-10-06 00:40:17 +0300

    refactor(parser): перенес в функции

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,7 +4,6 @@ import logging
 import os
 import re
 import sys
-from gettext import Catalog
 from time import sleep
 from urllib.parse import urljoin
 
@@ -15,10 +14,22 @@ from parser_response_tools import fetch_book_response
 from save_book_tools import save_to_file
 from tululu import parse_book_page
 
-BASE_URL = "https://tululu.org/"
+
+def get_book_url(cart) -> list:
+    link_selector = "a"
+    book_id = cart.select_one(link_selector)["href"]
+    link = urljoin(base_url, book_id)
+    return [link, book_id]
 
 
-def get_book_id(link: str) -> str:
+def save_books_to_json_file(user_folder, books):
+    os.makedirs(user_folder, exist_ok=True)
+    books_json = json.dumps(books, ensure_ascii=False, indent=3)
+    with open(os.path.join(user_folder, "books.json"), "w") as my_file:
+        my_file.write(books_json)
+
+
+def get_cleaned_book_id(link: str) -> str:
     math = re.search(r"\d+", link)
     if math:
         book_id = math.group()
@@ -55,7 +66,7 @@ def get_settings_parser():
     parser.add_argument(
         "--dest_folder",
         type=str,
-        default="",
+        default="books",
         help="путь к каталогу с книгами, обложками, JSON",
     )
     args = parser.parse_args()
@@ -70,13 +81,14 @@ def get_settings_parser():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-
     start_page, end_page, skip_txt, skip_img, dest_folder = get_settings_parser()
+    base_url = "https://tululu.org/"
+
     all_books = []
     for page in range(start_page, end_page + 1):
-        BOOKS_FANTASY_URL = urljoin("https://tululu.org/l55/", str(page))
+        books_fantasy_url = urljoin("https://tululu.org/l55/", str(page))
         try:
-            response = fetch_book_response(BOOKS_FANTASY_URL)
+            response = fetch_book_response(books_fantasy_url)
         except requests.HTTPError:
             logging.error("Заданная страница не существует")
             sys.exit()
@@ -89,21 +101,18 @@ if __name__ == "__main__":
             attempt = 0
             while attempt < retries:
                 try:
-                    link_selector = "a"
-                    part_link = cart.select_one(link_selector)["href"]
-                    link = urljoin(BASE_URL, part_link)
-                    response = fetch_book_response(link)
+                    book_url, book_id = get_book_url(cart)
+                    response = fetch_book_response(book_url)
                     title, author, image_path, comments, genres = parse_book_page(
                         response
                     )
-                    # Убираем из комментариев \n
+
                     cleaned_comments = get_cleaned_comments(comments)
-                    book_id = get_book_id(part_link)
-                    # Скачиваем книгу
+                    cleaned_book_id = get_cleaned_book_id(book_id)
 
                     download_book_url = "https://tululu.org/txt.php"
                     book = fetch_book_response(
-                        url=download_book_url, params={"id": book_id}
+                        url=download_book_url, params={"id": cleaned_book_id}
                     )
 
                     if book.content and not skip_txt:
@@ -111,10 +120,8 @@ if __name__ == "__main__":
                             content=book.content,
                             directory=dest_folder,
                             name_folder="books",
-                            file_name=f"{book_id}. {title}",
+                            file_name=f"{cleaned_book_id}. {title}",
                         )
-
-                    # Скачиваем изображение
 
                     cover_url = urljoin(response.url, image_path)
                     image = fetch_book_response(url=cover_url)
@@ -128,7 +135,6 @@ if __name__ == "__main__":
                             extension=img_ext,
                         )
 
-                    # Создаем json файл
                     book_path = f"Books/{title}.txt"
                     image_src = f"Image/{title}.{img_ext}"
                     all_books.append(
@@ -142,8 +148,11 @@ if __name__ == "__main__":
                         }
                     )
                     break
+
                 except requests.HTTPError:
-                    logging.info(f"Не удалось загрузить книгу с id = {str(book_id)}")
+                    logging.info(
+                        f"Не удалось загрузить книгу с id = {str(cleaned_book_id)}"
+                    )
                     attempt += 1
                     sleep(1)
                     continue
@@ -153,7 +162,4 @@ if __name__ == "__main__":
                     sleep(1)
                     continue
 
-    os.makedirs(dest_folder, exist_ok=True)
-    books_json = json.dumps(all_books, ensure_ascii=False, indent=3)
-    with open(os.path.join(dest_folder, "books.json"), "w") as my_file:
-        my_file.write(books_json)
+    save_books_to_json_file(dest_folder, all_books)
