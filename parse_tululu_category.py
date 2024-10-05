92906abeb04af5b9fabaecbeb4c79d0e6bb87c41
Stmkv <stepkamakarov1999@mail.ru>
2024-10-05 21:10:14 +0300

    fix(parser):Исправил ошибку, при которой не сбрасывался счетчик неудачных полдключений

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import re
+from time import sleep
 from urllib.parse import urljoin
 
 import requests
@@ -22,19 +23,19 @@ def get_book_id(link: str) -> str:
 
 
 start_page = 1
-end_page = 1
+end_page = 2
 #! TODO добавить обработку последней страницы
 json_books = []
-for _ in range(start_page, end_page + 1):
-    BOOKS_FANTASY_URL = urljoin("https://tululu.org/l55/", str(start_page))
+for page in range(start_page, end_page + 1):
+    BOOKS_FANTASY_URL = urljoin("https://tululu.org/l55/", str(page))
     response = fetch_book_response(BOOKS_FANTASY_URL)
 
     soup = BeautifulSoup(response.text, "lxml")
     carts_books = soup.find_all("div", class_="bookimage")
 
-    retries = 3
-    attempt = 0
     for cart in carts_books:
+        retries = 3
+        attempt = 0
         while attempt < retries:
             try:
                 part_link = cart.find("a")["href"]
@@ -42,11 +43,9 @@ for _ in range(start_page, end_page + 1):
                 response = fetch_book_response(link)
                 title, author, image_path, comments, genres = parse_book_page(response)
                 # Убираем из комментариев \n
-                feedbak = []
-                for comment in comments:
-                    if r"\n" in comment:
-                        comment = comment.replace(r"\n", "")
-                    feedbak.append(comment)
+                cleaned_comments = [
+                    comment.replace("\n", " ").replace('"', "'") for comment in comments
+                ]
 
                 book_id = get_book_id(part_link)
                 # Скачиваем книгу
@@ -54,22 +53,28 @@ for _ in range(start_page, end_page + 1):
                 book = fetch_book_response(
                     url=download_book_url, params={"id": book_id}
                 )
-                # save_to_file(book.content, "Books", f"{book_id}. {title}")
+
+                if book.content:
+                    save_to_file(book.content, "Books", f"{book_id}. {title}")
+                else:
+                    break
+
                 # Скачиваем изображение
                 cover_url = urljoin(response.url, image_path)
                 image = fetch_book_response(url=cover_url)
                 _, img_ext = tuple(image_path.split("."))
-                # save_to_file(image.content, "Image", title, extension=img_ext)
+                save_to_file(image.content, "Image", title, extension=img_ext)
 
                 # Создаем json файл
-                book_path = f"Books/{title}.{img_ext}"
+                book_path = f"Books/{title}.txt"
                 image_src = f"Image/{title}.{img_ext}"
                 json_books.append(
                     {
                         "title": title,
                         "author": author,
                         "image_src": image_src,
-                        "book_path" "comments": feedbak,
+                        "book_path": book_path,
+                        "comments": cleaned_comments,
                         "genres": genres,
                     }
                 )
@@ -77,10 +82,12 @@ for _ in range(start_page, end_page + 1):
             except requests.HTTPError:
                 logging.info(f"Не удалось загрузить книгу с id = {str(book_id)}")
                 attempt += 1
+                sleep(1)
                 continue
             except requests.ConnectionError:
                 logging.info("Не удалось подключиться к серверу")
                 attempt += 1
+                sleep(1)
                 continue
 
 books_json = json.dumps(json_books, ensure_ascii=False, indent=3)
