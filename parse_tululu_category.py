0915070fc50bd6a9f41400643a8b01822d3b2820
Stmkv <stepkamakarov1999@mail.ru>
2024-10-05 19:53:45 +0300

    feat(parser): Скачивает книги жанра фанатстики - в папку book - первую страницу

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,17 +1,43 @@
+import re
 from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
 
 from parser_response_tools import fetch_book_response
+from save_book_tools import save_to_file
+from tululu import parse_book_page
 
 BASE_URL = "https://tululu.org/"
 
-url = "https://tululu.org/l55/"
-response = fetch_book_response(url)
 
-soup = BeautifulSoup(response.text, "lxml")
-carts_books = soup.find_all("div", class_="bookimage")
+def get_book_id(link: str) -> str:
+    math = re.search(r"\d+", link)
+    if math:
+        book_id = math.group()
+    return book_id
 
-for id_book in carts_books:
-    part_link = id_book.find("a")["href"]
-    link = urljoin(BASE_URL, part_link)
+
+start_page = 1
+end_page = 1
+#! TODO добавить обработку последней страницы
+while start_page <= end_page:
+    BOOKS_FANTASY_URL = urljoin("https://tululu.org/l55/", str(start_page))
+    response = fetch_book_response(BOOKS_FANTASY_URL)
+
+    soup = BeautifulSoup(response.text, "lxml")
+    carts_books = soup.find_all("div", class_="bookimage")
+
+    for id_book in carts_books:
+        part_link = id_book.find("a")["href"]
+        link = urljoin(BASE_URL, part_link)
+        response = fetch_book_response(link)
+        title, author, image_path, comments, genres = parse_book_page(response)
+
+        book_id = get_book_id(part_link)
+        # Скачиваем книгу
+        download_book_url = "https://tululu.org/txt.php"
+        book = fetch_book_response(url=download_book_url, params={"id": book_id})
+        save_to_file(book.content, "Books", f"{book_id}. {title}")
+        # Скачиваем изображение
+
+    start_page += 1
